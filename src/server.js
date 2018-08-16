9d486f8e04ca29990ec41ebce6d3f3aa4d7dd151
vantonio <vale.residencia@gmail.com>
2018-08-16 16:25:20 -0500

    config server

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express  from 'express';
+const app  = express();
+
+
+app.set('port' , process.env.PORT || 3000);
+
+
+app.get('/' , (req, res) => {
+	res.send('Hello world')
+});
+
+app.listen(app.get('port') , () => {
+	console.log('server on port ', app.get('port'));
+})
