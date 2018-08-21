6d403e2767f102ac7caf0d2e881f8990ba5e7ecd
vantonio <vale.residencia@gmail.com>
2018-08-20 19:49:56 -0500

    firebase config

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
 
 
+import firebase from 'firebase';
+import { DB_CONFIG } from '../../config/config';
+import 'firebase/database';
+
+
+
 class Header extends Component {
+
+	constructor() {
+		super();
+
+		this.app = firebase.initializeApp(DB_CONFIG);
+		this.database  = this.app.database().ref().child('example');		
+	}
+
+	componentDidMount(){
+		this.database.on('value', snap => {
+			this.setState({
+				speed : snap.val()
+			})
+		})
+	}
+
+
 	render(){
 		return (
 		      <header>
-		      <div className="example">
-		      <img src="https://fmtu.mx/wp-content/uploads/2018/05/FM-TU-Logo-letras-blancas-02.png"/>
-		      <form>
-		      <input type="text" placeholder="Ingresa el codigo" />
-		      </form>
-			   </div>
-			</header>
+			    <div className="example">
+			      <img src="https://fmtu.mx/wp-content/uploads/2018/05/FM-TU-Logo-letras-blancas-02.png"/>
+			      <br/><br/>
+			      <form>
+			      <p>{this.state.speed}</p>
+                    <div className="form-group">
+                       <input type="text" className="form-control" placeholder="Ingresa el codigo" />
+                    </div>
+			       </form>
+				</div>
+			 </header>
 			)
 	}
 }
---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+ export const DB_CONFIG = {
+    apiKey: "AIzaSyBU9zUt8oFPKDSSV8Qsix3w9-cYrqW-PN0",
+    authDomain: "boletos-228ca.firebaseapp.com",
+    databaseURL: "https://boletos-228ca.firebaseio.com",
+    projectId: "boletos-228ca",
+    storageBucket: "boletos-228ca.appspot.com",
+    messagingSenderId: "693253878424"
+  }
